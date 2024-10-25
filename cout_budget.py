c131a7e8dc4988ba3fc70bffc311c9cb422301a4
Isoard Valentin <46758135+Numero7-Mojeangering@users.noreply.github.com>
2024-10-25 11:32:22 +0200

    cout bud

---
+++ cout_budget.py
@@ -0,0 +1,6 @@
+# paiements
+melissa_morris = 0.5 * 224 + (21 - 0.5) * 224 * 0.4
+melissa_cooper = 21  * 224 * 0.4
+corey_kelly    = 21  * 288 * 0.4
+
+print(melissa_morris+melissa_cooper+corey_kelly)
